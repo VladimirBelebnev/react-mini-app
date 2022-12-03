926c7e0b0d87186ebbfd4b7eda56e18eeb2c2f0b
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-12-03 10:31:43 +0300

    add photos app

--- src/App.js
+++ src/App.js
@@ -1,52 +1,68 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Collection } from './Collection';
 import './index.scss';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+const categories = [
+	{ "name": "Все" },
+	{ "name": "Море" },
+	{ "name": "Горы" },
+	{ "name": "Архитектура" },
+	{ "name": "Города" }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Моя коллекция фотографий</h1>
-      <div className="top">
-        <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
-        </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
-      </div>
-      <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
-      </ul>
-    </div>
-  );
-}
+	const [categoryID, setCategoryID] = useState(0);
+	const [collections, setCollections] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+	const [isLoading, setIsLoading] = useState(true);
+	const [page, setPage] = useState(1);
+
+	useEffect(() => {
+		setIsLoading(true);
+
+		const category = categoryID ? `category=${categoryID}` : ''; 
+
+		fetch(`https://6389f629c5356b25a20dc4dd.mockapi.io/collections?page=${page}&limit=3&${category}`)
+			.then(result => result.json())
+			.then(json => setCollections(json))
+			.catch(err => {
+				console.log(err);
+				alert('Ошибка загрузки данных с сервера.')
+			})
+			.finally(() => setIsLoading(false))
+	}, [categoryID, page]);
+
+	return (
+		<div className="App">
+		<h1>Моя коллекция фотографий</h1>
+		<div className="top">
+			<ul className="tags">
+				{
+					categories.map((obj, index) => <li onClick={() => setCategoryID(index)} className={categoryID == index ? 'active' : ''} key={obj.name}>{obj.name}</li>)
+				}
+			</ul>
+			<input 
+				value={searchValue} 
+				onChange={event => setSearchValue(event.target.value)}
+				className="search-input" 
+				placeholder="Поиск по названию" />
+		</div>
+		<div className="content">
+				{isLoading ? <h2>Идет загрузка...</h2> : 
+					collections
+						.filter(obj => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+						.map((obj, index) => (
+							<Collection key={index} name={obj.name} images={obj.photos} />)
+				)}
+		</div>
+			<ul className="pagination">
+				{
+					[...Array(3)].map((_, index) => <li onClick={() => setPage(index + 1)} className={page == index + 1 ? 'active' : ''}>{index + 1}</li>)
+				}
+			</ul>
+		</div>
+	);
+};
 
 export default App;
