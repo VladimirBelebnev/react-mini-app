90f3b82f3337229c0f2dba38d2d3120b9b93d2bd
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-12-03 10:29:32 +0300

    add modal app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Children } from 'react';
+import { useState } from 'react';
 import './index.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+const Modal = ({ openModal, setOpenModal, children }) => {
+    return (
+        <div className={`overlay ${openModal ? 'show' : 'animated'}`}>
+            <div className="modal">
+                <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setOpenModal(false)}>
+                    <title />
+                    <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+                </svg>
+                {children}
+            </div>
         </div>
-      </div> */}
-    </div>
-  );
+    )
+};
+
+function App() {
+    const [openModal, setOpenModal] = useState(false);
+
+    return (
+        <div className="App">
+            <button className="open-modal-btn" onClick={() => setOpenModal(true)}>✨ Открыть окно</button>
+            <Modal openModal={openModal} setOpenModal={setOpenModal}>
+                <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+            </Modal>
+        </div>
+    );
 }
 
 export default App;
